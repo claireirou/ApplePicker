dbc95d8af5f4df49bbe61e79f26c3083b36d78fa
Claire Irou <c.iroudayassamy@gmail.com>
2019-09-05 12:41:46 -0400

    Starting AppleTree Script
    Where I left off when class ended. Started on the script for AppleTree and getting ready to start the basic movement script.

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    //Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    //Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Dropping apples every second
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+        //Changing Direction
+    }
+}
