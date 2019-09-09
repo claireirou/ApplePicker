abd7c42118ea6d4fa459cb56f0af1045e4d01705
Claire Irou <c.iroudayassamy@gmail.com>
2019-09-09 16:11:14 -0400

    High scores and removing baskets
    High score counter now works and a basket is removed if an apple is missed.
    ApplePicker project is completed to the textbooks specs.

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,16 +10,20 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
+
         for(int i=0; i<numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -30,6 +35,23 @@ public class ApplePicker : MonoBehaviour
         foreach (GameObject tGO in tAppleArray)
         {
             Destroy(tGO);
+        }
+
+        // Destroy one of the baskets
+        // Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count - 1;
+
+        // Get a reference to that basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+
+        // Remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
+        // If there are no Baskets left, restart the game
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
         }
     }
 }
